fe2de9d799d8d36f5e8dcf38329eb5309e102102
Lev <levildar@gmail.com>
2020-11-05 17:38:49 +0500

    finish

--- dist/js/script.js
+++ dist/js/script.js
@@ -143,7 +143,7 @@ function setClock(selector, endtime) {
   // Classes
 
   class MenuCard {
-    constructor(src, alt, title, desc, price, parentSelector) {
+    constructor(src, alt, title, desc, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
@@ -152,6 +152,7 @@ function setClock(selector, endtime) {
       this.transfer = 83;
       this.changeToRUR();
       this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
     }
 
     changeToRUR() {
@@ -160,8 +161,13 @@ function setClock(selector, endtime) {
 
     render() {
       const element = document.createElement('div');
+      if (this.classes.length == 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
       element.innerHTML = `
-        <div class="menu__item">
           <img src=${this.src} alt=${this.alt}>
           <h3 class="menu__item-subtitle">${this.title}</h3>
           <div class="menu__item-descr">${this.desc}</div>
@@ -170,7 +176,6 @@ function setClock(selector, endtime) {
               <div class="menu__item-cost">Цена:</div>
               <div class="menu__item-total"><span>${this.price}</span> р/день</div>
           </div>
-        </div>
       `;
       this.parent.append(element);
     }
@@ -182,7 +187,9 @@ function setClock(selector, endtime) {
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    '.menu .container'
+    '.menu .container',
+    'menu__item',
+    'big'
   ).render();
 
   new MenuCard(
@@ -200,7 +207,8 @@ function setClock(selector, endtime) {
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     21,
-    '.menu .container'
+    '.menu .container',
+    'menu__item'
   ).render();
 });
 
