be3e750a8885dbf2ce167f89f955d142697fa02c
Lev <levildar@gmail.com>
2020-11-08 16:49:04 +0500

    fetch

--- js/script.js
+++ js/script.js
@@ -140,11 +140,11 @@ function setClock(selector, endtime) {
   // Classes
 
   class MenuCard {
-    constructor(src, alt, title, desc, price, parentSelector, ...classes) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
-      this.desc = desc;
+      this.descr = descr;
       this.price = price;
       this.transfer = 83;
       this.changeToRUR();
@@ -178,36 +178,46 @@ function setClock(selector, endtime) {
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    '.menu .container',
-    'menu__item',
-    'big'
-  ).render();
+  const getResource = async (url) => {
+    const res = await fetch(url);
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню "Премиум"',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    14,
-    '.menu .container'
-  ).render();
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${url.status}`);
+    }
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    21,
-    '.menu .container',
-    'menu__item'
-  ).render();
+    return await res.json();
+  };
 
+  // getResource('http://localhost:3000/menu')
+  // .then(data => {
+  //   data.forEach(({img, altimg, title, descr, price}) => {
+  //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //   });
+  // });
+
+  getResource('http://localhost:3000/menu')
+    .then(data => createCard(data));
+
+    function createCard(data) {
+      data.forEach(({img, altimg, title, descr, price}) => {
+        const element = document.createElement('div');
+
+        element.classList.add('menu__item');
+
+        element.innerHTML = `
+          <img src=${img} alt=${altimg}>
+          <h3 class="menu__item-subtitle">${title}</h3>
+          <div class="menu__item-descr">${descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${price}</span> р/день</div>
+          </div>
+        `;
+
+        document.querySelector('.menu .container').append(element);
+      });
+    }
   // Forms
 
   const forms = document.querySelectorAll('form');
@@ -219,10 +229,22 @@ function setClock(selector, endtime) {
   };
 
   forms.forEach(item => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: data
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -236,19 +258,9 @@ function setClock(selector, endtime) {
 
       const formData = new FormData(form);
 
-      const object = {};
-      formData.forEach(function(value, key){
-        object[key] = value;
-      });
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-      fetch('server2.php', {
-        method: "POST",
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(object)
-      })
-      .then(data => data.text())
+      postData('http://localhost:3000/requests', json)
       .then(data => {
         console.log(data);
           showThanksModal(message.success);
@@ -285,6 +297,10 @@ function setClock(selector, endtime) {
       closeModal();
     }, 4000);
   }
+
+  fetch('http://localhost:3000/requests')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
 
 
