b75aaf326e5d4a8e82571b5e9ee676b6a4c9f251
Lev <levildar@gmail.com>
2020-11-10 21:19:06 +0500

    modules

---
+++ js/modules/cards.js
@@ -0,0 +1,51 @@
+import {getResource} from '../services/services';
+
+function cards() {
+  
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.transfer = 83;
+      this.changeToRUR();
+      this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
+    }
+
+    changeToRUR() {
+      this.price = this.transfer * this.price;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      if (this.classes.length == 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+      element.innerHTML = `
+          <img src=${this.src} alt=${this.alt}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.desc}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> р/день</div>
+          </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+
+  getResource('http://localhost:3000/menu')
+  .then(data => {
+    data.forEach(({img, altimg, title, descr, price}) => {
+    new MenuCard(img, altimg, title, descr, price, '.menu .container').render();});
+  });
+}
+
+export default  cards;
